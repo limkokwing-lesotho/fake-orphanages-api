3f0fd5772be213b74608b570c85590b12ad18f7d
Ntholi Nkhatho <ntholi@live.com>
2022-11-21 14:53:16 +0200

    Allow CORS

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return """{ 
+        "name": "Thabo Lebese"
+    }"""
 
 if __name__ == '__main__':
     app.run()
